212ad7be4ca8e6a555ffb4f096013756b62c7874
Deadpool1 <atwood169@yahoo.com>
2023-06-01 16:44:14 -0500

    updated gameboard function

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,2 +1,32 @@
 const createShip = require('./ship')
 
+const gameboard = () => {
+    const grid = Array(10)
+    .fill()
+    .map(() => Array(10).fill({}))
+
+    const placeShip = (x, y, length, orientation) => {
+        const newShip = createShip(length)
+        const positions = []
+        for (let i = 0; i < length; i++) {
+            if (orientation === 'horizontal') {
+                positions.push({ x: x + i, y })
+            } else {
+                positions.push({ x, y: y + i })
+            }
+        }
+        positions.forEach((position) => {
+            grid[position.x][position.y] = { ship: newShip, status: 'notHit' }
+        })
+    }
+
+    return {
+        grid,
+        placeShip
+    }
+}
+
+const board = gameboard();
+board.placeShip(0, 0, 3, 'horizontal');
+
+module.exports = gameboard
