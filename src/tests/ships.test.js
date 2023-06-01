e96504c59368c36d459e89724a454fda869a9890
Deadpool1 <atwood169@yahoo.com>
2023-06-01 16:09:55 -0500

    added all ships tests

--- src/tests/ships.test.js
+++ src/tests/ships.test.js
@@ -5,3 +5,23 @@ test('Increases hit number by 1 when called', () => {
     testShip.hit()
     expect(testShip.getHitNum()).toBe(1)
 })
+
+test('Increases hit number by 2 when called', () => {
+    const testShip = createShip(3)
+    testShip.hit()
+    testShip.hit()
+    expect(testShip.getHitNum()).toBe(2)
+})
+
+test('Ship has been sunk', () => {
+    const testShip = createShip(2)
+    testShip.hit()
+    testShip.hit()
+    expect(testShip.isSunk()).toBe(true)
+})
+
+test('Ship is not sunk', () => {
+    const testShip = createShip(2)
+    testShip.hit()
+    expect(testShip.isSunk()).not.toBe(true)
+})
